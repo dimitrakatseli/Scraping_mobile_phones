50e05bf203fecd9574dda42887ab0419ec6d7b93
Dimitra Katseli <dimitraapeiranthos@gmail.com>
2019-04-11 16:13:41 +0300

    Fix file errors

--- Skroutz_items.py
+++ Skroutz_items.py
@@ -1,13 +1,14 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+import time
 
 #create save file
 filename = "mobile_phones.csv"
 f = open(filename,"w")
 headers = "title, brand, link, rating"
-f.write(headers)
-
-for page in range(1, 50):	
+f.write(headers +"\n")
+page=1
+for page in range(1, 10):	
 	my_url = 'https://www.skroutz.gr/c/40/kinhta-thlefwna.html?from=families&page=' +str(page)
 
 	#opening up connection and get the page
@@ -35,15 +36,17 @@ for page in range(1, 50):
 		#fix link
 		link = "https://www.skroutz.gr" + link 
 		#print(link)
-		rating = details.div.a.div.span
-		rating = rating.partition(">")[1] 
-		rating= rating.partition("<")[0]
-		#print(rating)
+		rating = details.div.a.div.span.text
+	    #print(rating)
 		brand = details.h2.a["title"].partition(" ")[0]
 		#print(brand)
 		print(title)
 		print(link)
-		print(rating)
+		print(str(rating))
 		print(brand)
-		#f.write(title + "" +brand + "" +link +"" +rating)
-f.close	
+		f.write(title + "," +brand + "," +link +","       +rating.replace(",",".") +"\n")
+
+	
+	print(my_url)
+ 
+f.close()  
