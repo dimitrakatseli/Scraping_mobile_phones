207e6735514567ee4ebe902a9c07c17f1a015777
Dimitra Katseli <dimitraapeiranthos@gmail.com>
2019-04-16 10:17:58 +0300

    Update skroutz_reviews.py
    Get reviews and save them to a file

--- skroutz_reviews.py
+++ skroutz_reviews.py
@@ -4,7 +4,12 @@ import time
 import csv
 
 filename = "mobile_phones.csv"
-
+file_review = "mobile_phones_review.csv"
+#create save file
+f = open(file_review,"a")
+headers = "title,brand,star,review"
+f.write(headers +"\n")
+sum=0
 
 with open(filename,'r') as csv_file:
 	csv_reader = csv.DictReader(csv_file, delimiter=',')
@@ -16,5 +21,40 @@ with open(filename,'r') as csv_file:
 		link = row[' link']
 
 		link = link + "#reviews"
-		print(link)
+
+		my_url = link
+
+		#opening up connection and get the page
+		
+		uClient = uReq(my_url)
+		page_html = uClient.read()
+		uClient.close()
+
+		#html parsing
+		page_soup = soup(page_html,"html.parser")
+		#graps its product
+
+		#get all card reviews
+		items = page_soup.findAll("li",{"class":"card review-item "})
+		
+		#for each card of reviews
+		for item in items:
+
+			#get review
+			review = item.p.text
+			print(review)
+
+			tmp_star = item.find("div",{"class":"rating-wrapper"})
+
+			star = tmp_star.span.text
+			print(star)
+		
+			f.write(title + "," +brand + "," +star.replace(",",".") +"," +review.replace(","," ") +"\n")
+			sum = sum + 1
+			print("sum",sum)
+			
+			
+		time.sleep(60)
+		
+	f.close()
 	
